a3fa99b54089e9986899a2f9568f4ccac008318b
Nikita Shah <NikitaS@baroda.sfwltd.co.uk>
2018-07-09 12:55:48 +0530

    student add/update

--- AngularStudentLib/components/student.ts
+++ AngularStudentLib/components/student.ts
@@ -2,28 +2,38 @@ import { IStudent } from "../interfaces/istudent";
 
 export class Student {
     student: IStudent = <IStudent>{};
-    allowedFiles:string[] = [".png", ".jpg", ".jpeg"];
-    regex:RegExp = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + this.allowedFiles.join('|') + ")$");
+    allowedFiles: string[] = [".png", ".jpg", ".jpeg"];
+    regex: RegExp = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + this.allowedFiles.join('|') + ")$");
     constructor() {
     }
+
+    get disabled(): boolean {
+        return (!this.student.studentName || !this.validateFileUpload(this.student.profilePicture, false));
+    }
     fileUploadEvent(files: FileList): void {
-        this.student.profilePicture=files.item(0);
-        if (!this.validateFileUpload(this.student.profilePicture)) {
-                this.student.profilePicture = null;
+        this.student.profilePicture = files.item(0);
+        if (!this.validateFileUpload(this.student.profilePicture, true)) {
+            this.student.profilePicture = null;
         }
     }
-    validateFileUpload(file: File): boolean {
-        if (file == undefined) {
-            alert("Profile Picture is required");
+    validateFileUpload(file: File, displayErrorMessage: boolean): boolean {
+        if (!file) {
+            if (displayErrorMessage) {
+                alert("Profile Picture is required");
+            }
             return false;
         }
         if (file.size <= 0) {
-            alert("profile picture is size must be grater than zero");
+            if (displayErrorMessage) {
+                alert("profile picture is size must be grater than zero");
+            }
             return false;
         }
 
         if (!this.regex.test(file.name.toLocaleLowerCase())) {
-            alert("Profile Picture must be jpeg,png file");
+            if (displayErrorMessage) {
+                alert("Profile Picture must be jpeg,png file");
+            }
             return false;
         }
 
